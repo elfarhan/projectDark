7623c820dca26124221fc5095afc1bee18c6a9b0
Liam Farhan <78386691+elfarhan@users.noreply.github.com>
2025-05-20 10:49:08 +0200

    yay

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
 @export_group("Movement")
-@export var max_speed = 4
+@export var max_speed = 400
 @export var acceleration = 6
 @export var jump_speed = 6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite2D.play("default")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity.y += 9.81 * delta
+	velocity.y += 900.081 * delta
 	move_and_slide()
