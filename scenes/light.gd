be6a51b1b35c0e6d01d49bb0d0586abad1ff0664
Liam Farhan <78386691+elfarhan@users.noreply.github.com>
2025-05-29 21:10:15 +0200

    actual tile map

--- scenes/light.gd
+++ scenes/light.gd
@@ -2,10 +2,11 @@ extends RigidBody2D
 
 
 var picked = true
-var playerNode = Player
 
 var player_node  # A reference to the player instance
 func _ready():
 	  # Adjust this path based on your actual scene tree
-	
-	self.position = playerNode.position
+	pass
+	#self.position = playerNode.position
+#@onready var player_node = $player  # or get_node("Player")
+#@onready var light_marker = player_node.get_node("LightMarker")
