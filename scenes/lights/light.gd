d3616b4c74936253e86d3e801f66d6eb4b180f44
Manuel Lata <contact@mlata.me>
2025-06-05 10:12:05 +0200

    make light size changes apply in the editor

--- scenes/lights/light.gd
+++ scenes/lights/light.gd
@@ -1,16 +1,27 @@
+@tool
 class_name Light
 extends RigidBody2D
 
 ## current radius of the light
-@export_range(0, 2048, 8) var radius = 256 
+@export_range(0, 2048, 8) var radius = 256:
+	set(new_radius):
+		radius = new_radius
+		if self.is_node_ready():
+			rescale_texture()
+@export var timeout = 0:
+	set(new_timeout):
+		timeout = new_timeout
+		$Timer.start(self.timeout)
+
 ## radius size change when dropped
 @export_range(0, 10, 0.125) var held_factor = 0.75
 
-@export var timeout = 0
 @onready var timer = $Timer
 @onready var light = $PointLight2D
 
 # TODO: investigate flickering
+const blur_width: float = 20
+
 func _ready():
 	light.enabled = true
 	# Start the timer when scene loads
@@ -20,11 +31,11 @@ func _ready():
 
 const scene = preload("res://scenes/lights/Light.tscn")
 static func create(radius, timeout, dynamic = true):
-	var light = scene.instantiate()
-	light.radius = radius
-	light.timeout = timeout
-	light.set_dynamic(dynamic)
-	return light
+	var out = scene.instantiate()
+	out.radius = radius
+	out.timeout = timeout
+	out.set_dynamic(dynamic)
+	return out
 
 func set_dynamic(flag):
 	if flag:
@@ -35,20 +46,8 @@ func set_dynamic(flag):
 		self.freeze = true
 		self.get_node(^"CollisionShape2D").disabled = true
 
-func shrink(amount):
-	self.radius -= amount
-	self.rescale_texture()
-
-func resize(radius):
-	self.radius = radius
-	self.rescale_texture()
-
 func rescale_texture():
 	light.texture_scale = radius / self.scale.x / 256
-
-func set_timeout(timeout):
-	self.timeout = timeout
-	$Timer.start(self.timeout)
 
 func _on_timer_timeout():
 	light.enabled = false
