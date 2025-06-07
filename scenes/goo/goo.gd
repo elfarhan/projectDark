f90c11c4a053d76914a7dec54bbaa9c851a90f42
Liam Farhan <78386691+elfarhan@users.noreply.github.com>
2025-06-07 18:42:50 +0200

    attach to node or position

--- scenes/goo/goo.gd
+++ scenes/goo/goo.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var attach_to_node: Node2D = null
 @onready var player_node = $"../Player"
 var goo = Goo.new()
 func _ready():
@@ -7,7 +8,10 @@ func _ready():
 	# When added to the scene, set up using own position as anchor
 	if player_node == null:
 		player_node = get_tree().get_root().get_node("Main/Player")  # Adjust path!
-	goo.initialize(player_node, $"../Light", $"../Light".global_position)
+	if attach_to_node == null:
+		goo.initialize(player_node, attach_to_node, global_position)
+	else:
+		goo.initialize(player_node, attach_to_node)
 	
 	add_child(goo)
 
